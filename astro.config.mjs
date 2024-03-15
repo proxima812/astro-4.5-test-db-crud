1d874a0ddfc987ae0f8ce2463fd30a7bde51e76d
proxima812 <mrzuc2015@yandex.ru>
2024-03-15 15:26:39 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import db from "@astrojs/db"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [db(), tailwind()],
 	output: "server",
+	adapter: netlify(),
 })
